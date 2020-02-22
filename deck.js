39c45e2b70a3de0e5ff315cbe494d66ee2a357e0
Megan Huggins <Muggins1265@gmail.com>
2020-02-21 18:00:54 -0700

    Finish removeCard method

--- deck.js
+++ deck.js
@@ -1,26 +1,25 @@
 class Deck {
-	constructor(cardArray) {
-	this.cards = cardArray;
-	this.matchedCards = cardArray.matchedInfo;
-  this.selectedCards = [];
-  this.matchedArray = [];
-	}
-	shuffle() {
-	//Creates a new array each time its ran
-  // var display = document.querySelector('.place-holder-card');
-  // var button = document.querySelector('.play-buttons');
-  // button.addEventListener('click', random);
-  // function random(){
-  // var randomItem =  myArray[Math.floor(Math.random()*myArray.length)];
-  // return display.innerHTML = randomItem
-};
-  addSelected(selectedId){
+  constructor(cardArray) {
+    this.cards = cardArray;
+    this.matchedCards = cardArray.matchedInfo;
+    this.selectedCards = [];
+    this.matchedArray = [];
+  }
+  shuffle() {
+    //Creates a new array each time its ran
+    // var display = document.querySelector('.place-holder-card');
+    // var button = document.querySelector('.play-buttons');
+    // button.addEventListener('click', random);
+    // function random(){
+    // var randomItem =  myArray[Math.floor(Math.random()*myArray.length)];
+    // return display.innerHTML = randomItem
+  };
+  addSelected(selectedId) {
     selectedId--;
-    if (this.selectedCards.length < 2){
-    this.selectedCards.push(this.cards[selectedId]);
-    this.cards[selectedId].pushSelected();
+    if (this.selectedCards.length < 2) {
+      this.selectedCards.push(this.cards[selectedId]);
+      this.cards[selectedId].toggleSelected();
     }
-
     console.log("blarh", this.selectedCards);
     // for (var i = 0; i < this.cards.length; i++){
     //   if (this.cards[i].cardid == selectedId){
@@ -28,30 +27,30 @@ class Deck {
     //   }
   };
 
-  removeSelected(selectedId){
-    console.log(selectedId)
-    if (this.selectedCards.length === 2){
-      if (this.selectedCards[0].cardId === selectedId){
-            this.selectedCards.shift();
-          } else if(this.selectedCards[1].cardId === selectedId){
-            this.selectedCards.pop();
-          }
-    }
+  removeSelected(selectedId) {
+    selectedId--;
+    if (this.selectedCards[0].cardId === selectedId + 1){
+    this.cards[selectedId].toggleSelected();
     this.selectedCards.shift();
+  } else if (this.selectedCards[1].cardId === selectedId + 1){
+    this.cards[selectedId].toggleSelected();
+    this.selectedCards.pop();
   }
-  
-	checkSelectedCards(select1, select2) {
-	//take each selected card and run isMatched()
-  if (select1.matchedInfo === select2.matchedInfo) {
-    select1.match();
-    select2.match();
-    this.movetoMatched(select1, select2);
+    console.log('okurrrr', this.selectedCards)
+  }
+
+  checkSelectedCards(select1, select2) {
+    //take each selected card and run isMatched()
+    if (select1.matchedInfo === select2.matchedInfo) {
+      select1.match();
+      select2.match();
+      this.movetoMatched(select1, select2);
     }
-	};
-	moveToMatched(select1, select2) {
+  };
+  moveToMatched(select1, select2) {
     this.matchedArray.push(select1);
     this.matchedArray.push(select2);
-	//take matched pairs out of array
-	//place matched pair into found matches area
+    //take matched pairs out of array
+    //place matched pair into found matches area
   };
 }
