2b0ae4f8c9f07c87940265b677d12cf1b4567ea8
Megan Huggins <Muggins1265@gmail.com>
2020-02-25 17:31:13 -0700

    Fix bugs with shuffle method and refactor main.js

--- deck.js
+++ deck.js
@@ -9,71 +9,42 @@ class Deck {
     var i = cardArray.length;
     var j; //random number
     var temp; //random index position
-  
+
     while(--i > 0){ //i decrements by one until the 0 index
       j = Math.floor(Math.random() * (i+1)); //generates random number
       temp = cardArray[j]; //plugs random number into index position in array
       cardArray[j] = cardArray[i]; //reassigns random index value with current index position in loop
       cardArray[i] = temp; //reassigns index position's value in loop to random index value
     }
-    return cardArray 
+    return cardArray
   };
 
-  addSelected(selectedId) {
-    selectedId--;
-    if (this.selectedCards.length < 2) {
-      this.selectedCards.push(this.cards[selectedId]);
-      this.cards[selectedId].toggleSelected();
-    }
-    if (this.selectedCards.length === 2) {
-      this.checkSelectedCards()
-      };
-    console.log("selectedCards", this.selectedCards);
-    // for (var i = 0; i < this.cards.length; i++){
-    //   if (this.cards[i].cardid == selectedId){
-    //     card = this.cards[i];
-    //   }
-
+  addSelected(cardLoc) {
+    this.selectedCards.push(this.cards[cardLoc]);
+    console.log('addSelectedMethod', this.selectedCards);
   };
 
-  removeSelected(selectedId) {
-    console.log('selectedID bug', selectedId);
-    console.log('before remove bug', this.selectedCards);
-    var cardIndex = this.selectedCards.indexOf(selectedId);
-    this.cards[selectedId-1].toggleSelected();
-    this.selectedCards.splice(cardIndex,1);
-    console.log('remove bug', this.selectedCards);
-  //   if (this.selectedCards[0].cardId === selectedId + 1){
-  //   this.cards[selectedId].toggleSelected();
-  //   this.selectedCards.shift();
-  // } else if (this.selectedCards[1].cardId === selectedId + 1){
-  //   this.cards[selectedId].toggleSelected();
-  //   this.selectedCards.pop();
-  // }
-  //   console.log('selectedCards', this.selectedCards);
-  };
+  // removeSelected() {
+  //   this.selectedCards = [];
+  //   var cardIndex = this.selectedCards.indexOf(selectedId);
+  //   this.cards[selectedId-1].toggleSelected();
+  //   this.selectedCards.splice(cardIndex,1);
+  //   console.log('remove bug', this.selectedCards);
+  // };
 
   checkSelectedCards() {
-    //take each selected card and run isMatched()
     if (this.selectedCards[0].matchedInfo === this.selectedCards[1].matchedInfo) {
       this.selectedCards[0].match();
       this.selectedCards[1].match();
       this.moveToMatched();
-      this.selectedCards =[];
-    } else {
-      autoFlip();
-    }
-    console.log('matchedArray', this.matchedArray);
-    console.log(this.cards);
-    return
+      console.log('selectedCardsArray',this.selectedCards);
+    };
+      console.log('selectedCardsArrayAfter',this.selectedCards);
   };
 
   moveToMatched() {
     this.matchedArray.push(this.selectedCards[0]);
     this.matchedArray.push(this.selectedCards[1]);
-    this.selectedCards[0].toggleSelected();
-    this.selectedCards[1].toggleSelected();
-    //take matched pairs out of array
-    //place matched pair into found matches area
+    console.log('matchedArray', this.matchedArray);
   };
 }
